b1a320c9d0bd71740f2420334d8a80f889903cfc
acradibotC_tuananh <nguyentuanahh020601@gmail.com>
2023-03-22 23:25:00 +0700

    Equality in Hibernate

--- spring-6-webapp/src/main/java/com/acadibotc/springframework/spring6webapp/domain/Author.java
+++ spring-6-webapp/src/main/java/com/acadibotc/springframework/spring6webapp/domain/Author.java
@@ -1,6 +1,8 @@
 package com.acadibotc.springframework.spring6webapp.domain;
 
 import jakarta.persistence.*;
+
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -44,5 +46,30 @@ public class Author {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "Id=" + Id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return Objects.equals(Id, author.Id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Id != null ? Id.hashCode() : 0;
     }
 }
